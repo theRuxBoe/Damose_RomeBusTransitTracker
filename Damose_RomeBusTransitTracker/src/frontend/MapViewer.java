8bd479957e615945563eaa31f821d2a99e76ba75
Brother_Boe <francescoboero9051@gmail.com>
2025-10-01 08:57:06 +0200

    First commit

---
+++ Damose_RomeBusTransitTracker/src/frontend/MapViewer.java
@@ -0,0 +1,30 @@
+package frontend;
+
+import java.awt.Frame;
+
+//import java.awt.Frame;
+
+import javax.swing.*;
+
+public class MapViewer {
+	
+	private JFrame frame;
+
+	public void setFrame() {
+		JFrame frame = new JFrame();
+		this.frame = frame;
+		JButton b = new JButton();
+		frame.add(b);
+		frame.pack();
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+		
+	public static void main(String[] args) {
+		MapViewer a = new MapViewer();
+		a.setFrame();
+	
+	}
+		
+		
+}
